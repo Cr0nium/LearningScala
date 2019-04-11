21cfc7c4c0aedf715083023f46028cf5a158f9a9
t.zalimov <t.zalimov@deltasolutions.ru>
2019-04-11 09:56:24 +0300

    5.7 done

--- src/main/scala/scalaForTheImpatient/Four/Five/PersonNextStage.scala
+++ src/main/scala/scalaForTheImpatient/Four/Five/PersonNextStage.scala
@@ -1,5 +1,9 @@
 package scalaForTheImpatient.Four.Five
 
-class PersonNextStage(val name: String, var age: Int) {
-if (age < 0) age = 0
+class PersonNextStage(fio: String) {
+private  val fioSplit = fio.split("\\s")
+val firstName: String = fioSplit.head
+val lastName: String = fioSplit.last
+  def current = (firstName, lastName)
+
 }
--- src/main/scala/scalaForTheImpatient/Four/Five/Six.scala
+++ src/main/scala/scalaForTheImpatient/Four/Five/Six.scala
@@ -2,6 +2,7 @@ package scalaForTheImpatient.Four.Five
 
 object Six {
   def main(args: Array[String]): Unit = {
-
+val test = new PersonNextStage("Tim Zalim")
+    println(test.current)
   }
 }
